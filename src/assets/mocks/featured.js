6b3f4c20fa919211e2244faf3d099ed719324054
Jyotirmay Pathak <jyotirmay362@gmail.com>
2021-03-15 15:40:59 +0530

    front end completed

---
+++ src/assets/mocks/featured.js
@@ -0,0 +1,32 @@
+import moment from 'moment'
+
+export default [
+    {
+        title: 'Coding for everybody',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture', 'Tech News'],
+        link: '#',
+        image: 'coding.png',
+    },
+    {
+        title: 'React Vs Vue',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['React', 'Vue'],
+        link: '#',
+        image: 'react.jpeg'
+    },
+    {
+        title: 'Popular Programming Languages in 2020',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech News', 'Tech Culture'],
+        link: '#',
+        image: 'puzzled_programmer.jpeg'
+    },
+    {
+        title: 'DNA-the universal blueprint of life',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Brain Health'],
+        link: '#',
+        image: 'dna.jpeg'
+    },
+]
---
+++ src/assets/mocks/trending.js
@@ -0,0 +1,39 @@
+import moment from 'moment'
+
+export default [
+    {
+        title: 'Software Engineer Salary in 2020',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture'],
+        link: '#',
+        image: 'money.jpeg'
+    },
+    {
+        title: 'GraphQL vs REST',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['React', 'JavaScript'],
+        link: '#',
+        image: 'laptop.jpg',
+    },
+    {
+        title: 'A Day in the Life of a Programmer',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture'],
+        link: '#',
+        image: 'programming_languages.jpeg'
+    },
+    {
+        title: 'The future of cryptocurrency',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Brain Health'],
+        link: '#',
+        image: 'cryptocurrency.jpeg'
+    },
+    {
+        title: 'AWS- the most secure place for your data',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Cloud'],
+        link: '#',
+        image: 'aws.png',
+    },
+]
